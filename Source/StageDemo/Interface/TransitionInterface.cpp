76fd508f3f2f5ea1fcc6c510ee06e4db2f9e43c1
LingtaoLi <332305429@qq.com>
2025-09-11 15:55:35 +0800

    测试建筑跳转

---
+++ Source/StageDemo/Interface/TransitionInterface.cpp
@@ -0,0 +1,7 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TransitionInterface.h"
+
+
+// Add default functionality here for any ITransitionInterface functions that are not pure virtual.
---
+++ Source/StageDemo/Interface/TransitionInterface.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StageDemo/Common/Type.h"
+#include "UObject/Interface.h"
+#include "TransitionInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(BlueprintType)
+class UTransitionInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class STAGEDEMO_API ITransitionInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Transition2Target(ECameraView TargetView,FVector TargetPosition);
+	virtual void Transition2Target_Implementation(ECameraView TargetView,FVector TargetPosition)
+	{
+		UE_LOG(LogTemp, Display, TEXT("Transition2Parking"));
+	}
+};
